7d6b7a43606c1081e4bab998385160ac0a582342
Matthieu Vachon <matt@streamingfast.io>
2025-06-06 11:50:44 -0400

    Trying to avoid Stephane's warnings

--- cgo.go
+++ cgo.go
@@ -8,8 +8,8 @@ package v8go
 
 // #cgo CXXFLAGS: -fno-rtti -fPIC -std=c++17 -DV8_COMPRESS_POINTERS -DV8_31BIT_SMIS_ON_64BIT_ARCH -I${SRCDIR}/deps/include -Wall -DV8_ENABLE_SANDBOX
 // #cgo LDFLAGS: -pthread -lv8
-// #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/deps/darwin_x86_64
-// #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/deps/darwin_arm64
+// #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/deps/darwin_x86_64 -Wno-vla-cxx-extension
+// #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/deps/darwin_arm64 -Wno-vla-cxx-extension
 // #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/deps/linux_x86_64 -ldl
 // #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/deps/linux_arm64 -ldl
 import "C"
